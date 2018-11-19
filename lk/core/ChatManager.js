3fb1509e365529db6cc5d971d2e8ac454da25859
randomyige <randomyige@gmail.com>
2018-11-19 14:25:06 +0800

    tmp

--- lk/core/ChatManager.js
+++ lk/core/ChatManager.js
@@ -363,11 +363,9 @@ class ChatManager extends EventTarget{
     /**
      * clear recent list
      */
-    clear(){
-        LKChatHandler.asyClear(Application.getCurrentApp().getCurrentUser().id).then(()=>{
-            this.fire("recentChanged");
-        });
-
+    async clear(){
+        await LKChatHandler.asyClear(Application.getCurrentApp().getCurrentUser().id);
+        this.fire("recentChanged");
     }
 
     /**
